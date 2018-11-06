6cc19ce2fbeb4b4784707cf7e713e87f27b75ef6
sansuiran@gmail.com <coder2free@gmail.com>
2018-11-06 21:43:04 +0800

    update

--- java-rank-base/src/main/java/com/rank/base/map/CrunchifyConcurrentHashMapVsSynchronizedMap.java
+++ java-rank-base/src/main/java/com/rank/base/map/CrunchifyConcurrentHashMapVsSynchronizedMap.java
@@ -60,13 +60,7 @@ public class CrunchifyConcurrentHashMapVsSynchronizedMap {
                 });
             }
 
-            // Initiates an orderly shutdown in which previously submitted tasks are executed, but no new tasks will be accepted. Invocation
-            // has no additional effect if already shut down.
-            // This method does not wait for previously submitted tasks to complete execution. Use awaitTermination to do that.
             crunchifyExServer.shutdown();
-
-            // Blocks until all tasks have completed execution after a shutdown request, or the timeout occurs, or the current thread is
-            // interrupted, whichever happens first.
             crunchifyExServer.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
 
             long entTime = System.nanoTime();
